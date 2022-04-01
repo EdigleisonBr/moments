881dff6eb506d6daf918755374fabf6d97defc41
EdigleisonBr <edigleisonbr@gmail.com>
2022-04-01 14:08:05 -0300

    Validation with Reactive

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-moment-form',
-  templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+    selector: 'app-moment-form',
+    templateUrl: './moment-form.component.html',
+    styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
     @Input() btnText!: string
 
-  constructor() { }
+    momentForm!: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    // Quando se inicializa coisas do Angular
+    ngOnInit(): void {
+        // Declara todos os campos que tem no Formulário
+        this.momentForm = new FormGroup({
+            id: new FormControl(''),
+            title: new FormControl('', [Validators.required]),
+            description: new FormControl('', [Validators.required]),
+            image: new FormControl('')
+        });
+    }
+
+    get title() {
+        return this.momentForm.get('title')!;
+    }
+
+    get description() {
+        return this.momentForm.get('description')!;
+    }
+
+    submit() {
+        if (this.momentForm.invalid) {
+            return;
+        }
+        alert('Enviou formulário');
+    }
 
 }
